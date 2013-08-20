27875095764b392a91f042d9be2c2bda58b13ccb
Jason Bryan <Jmbryan10@gmail.com>
2013-08-19 20:00:32 -0700

    Initial CMakeLists

---
+++ include/Vector3.h
@@ -0,0 +1,13 @@
+#pragma once
+
+class Vector3
+{
+public:
+	Vector3();
+	~Vector3();
+	
+	float x;
+	float y;
+	float z;
+	
+};
---
+++ src/Vector3.cpp
@@ -0,0 +1,9 @@
+#include "Vector3.h"
+
+Vector3::Vector3()
+{
+}
+
+Vector3::~Vector3()
+{
+}
